60a55955ab5d9e8679227e9b0ea73ccb3643da67
ArthurDenis62 <arthurdenis.pro@gmail.com>
2023-04-01 03:55:43 +0200

    Setup

---
+++ assets/js/navigation.js
@@ -0,0 +1,65 @@
+$(function() {
+    $('#accueil').click(function() {
+      $.ajax({
+        url: 'Accueil.html',
+        success: function(data) {
+          $('#ContenuGlobal').html(data);
+        }
+      });
+    });
+  });
+
+  $(function() {
+    $('#boiteReception').click(function() {
+      $.ajax({
+        url: 'BoiteReception.html',
+        success: function(data) {
+          $('#ContenuGlobal').html(data);
+        }
+      });
+    });
+  });
+
+  $(function() {
+    $('#gestion').click(function() {
+      $.ajax({
+        url: 'Gestion.html',
+        success: function(data) {
+          $('#ContenuGlobal').html(data);
+        }
+      });
+    });
+  });
+
+  $(function() {
+    $('#projets').click(function() {
+      $.ajax({
+        url: 'Projets.html',
+        success: function(data) {
+          $('#ContenuGlobal').html(data);
+        }
+      });
+    });
+  });
+
+  $(function() {
+    $('#seConnecter').click(function() {
+      $.ajax({
+        url: 'seConnecter.html',
+        success: function(data) {
+          $('#ContenuGlobal').html(data);
+        }
+      });
+    });
+  });
+
+  $(function() {
+    $('#comptes').click(function() {
+      $.ajax({
+        url: 'Comptes.html',
+        success: function(data) {
+          $('#ContenuGlobal').html(data);
+        }
+      });
+    });
+  });
