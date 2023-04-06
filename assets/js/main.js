16ab823f4e6fc230845e783fec72598b29d8e577
ArthurDenis62 <arthurdenis.pro@gmail.com>
2023-04-06 20:27:09 +0200

    f

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,45 @@ popUpNotesPlus.addEventListener('click', function(event) {
 closeNotes.addEventListener('click', function() {
   popUpNotes.style.display = 'none';
 });
+
+const nomInput = document.getElementById("nom");
+const messageTextarea = document.getElementById("message");
+const validerBtn = document.getElementById("valider");
+const tableau = document.getElementById("tableau").getElementsByTagName('tbody')[0];
+
+// Récupérer les données du localStorage et les ajouter dans le tableau au chargement de la page
+for (let i = 0; i < localStorage.length; i++) {
+  const key = localStorage.key(i);
+  if (key.startsWith("nom_")) {
+    const nom = localStorage.getItem(key);
+    const message = localStorage.getItem(`message_${key.substring(4)}`);
+    ajouterLigne(nom, message);
+  }
+}
+
+validerBtn.addEventListener("click", () => {
+  const nom = nomInput.value;
+  const message = messageTextarea.value;
+
+  // Ajouter une nouvelle ligne au tableau et stocker les données dans le localStorage
+  ajouterLigne(nom, message);
+  localStorage.setItem(`nom_${Date.now()}`, nom);
+  localStorage.setItem(`message_${Date.now()}`, message);
+
+  // Effacer les champs du formulaire après validation
+  nomInput.value = '';
+  messageTextarea.value = '';
+});
+
+function ajouterLigne(nom, message) {
+  // Ajouter une nouvelle ligne au tableau
+  const nouvelleLigne = tableau.insertRow(-1);
+
+  // Insérer une cellule pour le nom dans la nouvelle ligne
+  const celluleNom = nouvelleLigne.insertCell(0);
+  celluleNom.innerHTML = nom;
+
+  // Insérer une cellule pour le message dans la nouvelle ligne
+  const celluleMessage = nouvelleLigne.insertCell(1);
+  celluleMessage.innerHTML = message;
+}
