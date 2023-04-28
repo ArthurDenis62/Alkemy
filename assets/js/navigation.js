4eb130e0a86fb5f7b26ef04a6e6ae92988ed2347
ArthurDenis62 <arthurdenis.pro@gmail.com>
2023-04-28 17:42:08 +0200

    Fix

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,76 +1,10 @@
 $(function() {
-    $('#accueil').click(function() {
-      $.ajax({
-        url: 'Accueil.html',
-        success: function(data) {
-          $('#ContenuGlobal').html(data);
-        }
-      });
+  $('#carteMental').click(function() {
+    $.ajax({
+      url: './GestionModule/CarteMental.html',
+      success: function(data) {
+        $('#ContenuGlobal').html(data);
+      }
     });
   });
-
-  $(function() {
-    $('#boiteReception').click(function() {
-      $.ajax({
-        url: 'BoiteReception.html',
-        success: function(data) {
-          $('#ContenuGlobal').html(data);
-        }
-      });
-    });
-  });
-
-  $(function() {
-    $('#gestion').click(function() {
-      $.ajax({
-        url: 'Gestion.html',
-        success: function(data) {
-          $('#ContenuGlobal').html(data);
-        }
-      });
-    });
-  });
-
-  $(function() {
-    $('#projets').click(function() {
-      $.ajax({
-        url: 'Projets.html',
-        success: function(data) {
-          $('#ContenuGlobal').html(data);
-        }
-      });
-    });
-  });
-
-  $(function() {
-    $('#seConnecter').click(function() {
-      $.ajax({
-        url: 'seConnecter.html',
-        success: function(data) {
-          $('#ContenuGlobal').html(data);
-        }
-      });
-    });
-  });
-
-  $(function() {
-    $('#comptes').click(function() {
-      $.ajax({
-        url: 'Comptes.html',
-        success: function(data) {
-          $('#ContenuGlobal').html(data);
-        }
-      });
-    });
-  });
-
-  $(function() {
-    $('#mesTaches').click(function() {
-      $.ajax({
-        url: 'MesTaches.html',
-        success: function(data) {
-          $('#ContenuGlobal').html(data);
-        }
-      });
-    });
-  });
+});
